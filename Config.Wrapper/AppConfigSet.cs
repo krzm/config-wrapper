485e2f730972f8c216ae33458c8f3cf79b6aa093
krzm <kmazanek@gmail.com>
2022-03-14 23:13:40 +0100

    Add DependencySet

---
+++ Config.Wrapper/AppConfigSet.cs
@@ -0,0 +1,25 @@
+using DIHelper.Unity;
+using Microsoft.Extensions.Configuration;
+using Unity;
+
+namespace Config.Wrapper;
+
+public class AppConfigSet 
+    : UnityDependencySet
+{
+    public AppConfigSet(
+        IUnityContainer container) 
+        : base(container)
+    {
+    }
+
+    public override void Register()
+    {
+        Container.RegisterSingleton<IConfigurationBuilder, ConfigurationBuilder>();
+        Container.RegisterSingleton<IDirectorySys, DirectorySys>();
+        Container.RegisterSingleton<IConfigBuilder, ConfigBuilder>();
+        Container.RegisterInstance<IConfiguration>(
+            Container.Resolve<IConfigBuilder>().BuildConfig());
+        Container.RegisterSingleton<IConfigReader, ConfigReader>();
+    }
+}
